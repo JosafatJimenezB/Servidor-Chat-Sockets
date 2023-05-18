1907846aace1c2407e8aa55fd93a280c913d021f
josafat <josa221001@gmail.com>
2023-05-17 22:11:02 -0600

    fix: problema de cors

--- src/main/java/com/chat/ChatApplication.java
+++ src/main/java/com/chat/ChatApplication.java
@@ -2,8 +2,10 @@ package com.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = "*")
 public class ChatApplication {
 
 	public static void main(String[] args) {
